2bc3d49c554732ae64b0c7afb81e2e22ef102e21
watanabe0916 <wtnbkut916@docomo.ne.jp>
2026-05-12 07:27:07 +0900

    状態管理ファイル追加

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { create } from 'zustand';
+
+export const useStore = create((set) => ({
+    nodes: [],
+    edges: [],
+    // グラフデータを更新する関数
+    updateGraph: (nodes, edges) => set({ nodes, edges }),
+}));
